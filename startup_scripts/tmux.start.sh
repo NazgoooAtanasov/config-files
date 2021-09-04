07f392707e935baf5eac507e3d49b76d7809682f
Nazgooo Atanasov <naskodragomirov@gmail.com>
2021-09-04 15:45:37 +0300

    Added tmux starup file

---
+++ startup_scripts/tmux.start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Basically add whatever commands you want to execute on login
+
+tmux start-server
+tmux new-session -d -n "PLACEHOLDER"
+tmux neww -n "PLACEHOLDER"
+tmux neww -n "PLACEHOLDER"
